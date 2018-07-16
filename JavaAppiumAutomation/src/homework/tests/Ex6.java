8e30fe006ec7f866f70d2226ef9dd90c4092cf9c
Bogdan Shamukov <mr.shamukov@gmail.com>
2018-07-16 23:05:56 +0300

    Add Ex6_1

--- JavaAppiumAutomation/src/homework/tests/Ex6.java
+++ JavaAppiumAutomation/src/homework/tests/Ex6.java
@@ -1,21 +1,21 @@
-package homework.tests;
-
-import homework.lib.MainConfig;
-import homework.lib.ui.ArticleMethods;
-import homework.lib.ui.SearchMethods;
-import org.junit.Test;
-
-public class Ex6 extends MainConfig {
-
-    @Test
-    public void testTitleIsExistOnArticle() {
-
-        SearchMethods SearchMethods = new SearchMethods(driver);
-        SearchMethods.clickSearchInput();
-        SearchMethods.typeSearchInput("Brest");
-        SearchMethods.clickByArticleWithSubstring("City in Belarus");
-
-        ArticleMethods ArticleMethods = new ArticleMethods(driver);
-        ArticleMethods.findTitleOnPage();
-    }
-}
+//package homework.tests;
+//
+//import homework.lib.MainConfig;
+//import homework.lib.ui.ArticleMethods;
+//import homework.lib.ui.SearchMethods;
+//import org.junit.Test;
+//
+//public class Ex6 extends MainConfig {
+//
+//    @Test
+//    public void testTitleIsExistOnArticle() {
+//
+//        SearchMethods SearchMethods = new SearchMethods(driver);
+//        SearchMethods.clickSearchInput();
+//        SearchMethods.typeSearchInput("Brest");
+//        SearchMethods.clickByArticleWithSubstring("City in Belarus");
+//
+//        ArticleMethods ArticleMethods = new ArticleMethods(driver);
+//        ArticleMethods.findTitleOnPage();
+//    }
+//}
